1c7345f63a6398248433a472069d446d47b8c605
Santiago Herrera <santiherr32@gmail.com>
2022-07-04 11:25:06 -0500

    Added file for practice examples of reassignment lesson

---
+++ src/scope/reassignment.js
@@ -0,0 +1,33 @@
+var firstName;
+firstName = "Santiago";
+console.log(firstName);
+
+var secondName = "Carlos";
+secondName = "Luis";
+console.log(secondName);
+
+var secondName = "Pedro";
+console.log(secondName);
+
+//Let
+let fruit = "Pear";
+fruit = "Orange";
+console.log(fruit);
+
+// let fruit = 'Banana'
+// console.log(fruit)
+
+//Const
+const country = "Russia";
+// country = "China";
+console.log(country);
+
+// const country = 'France';
+// console.log(country)
+
+const cars = [];
+cars.push("Tesla model S");
+console.log(cars);
+
+cars.pop();
+console.log(cars);
